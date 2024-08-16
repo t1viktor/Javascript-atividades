b718d6590996cf7eff18d3935d8d5779e9b251ca
Victor Lima <154839860+t1viktor@users.noreply.github.com>
2024-08-16 19:14:25 -0300

    Add files via upload

---
+++ aula-01/aula-01.js
@@ -0,0 +1,11 @@
+let n1 = 8.5;
+let n2 = 7.0;
+let n3 = 9.2;
+
+let p1 = 2;
+let p2 = 3;
+let p3 = 5;
+
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+console.log("A média ponderada é: " + media.toFixed(2));
